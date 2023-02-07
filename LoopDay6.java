22c30b6de6e610881810116ade86cf86ecf8f15a
SonamSherpa122 <124115433+SonamSherpa122@users.noreply.github.com>
2023-02-06 21:44:42 -0500

    Add files via upload

---
+++ LoopDay6.java
@@ -0,0 +1,13 @@
+public class LoopDay6
+{
+    public static void main(String[] args) {
+        System.out.println("Begin Numbers");
+        int x=1;
+
+        while(x<=100){
+            System.out.println("These are the Numbers....."+x);
+            x=x+1;
+        }
+        System.out.println("End Of the main");
+    }
+}
